5a7b88a30538cf76e509aac274196ca81ab08d18
Andrew <kad.io.work@gmail.com>
2026-03-11 23:12:53 +0300

    add hm6_2

--- hm6.py
+++ hm6.py
@@ -1,27 +1,35 @@
 def hm6():
-    # job 1. Заменить символ “#” на символ “/” в строке 'www.my_site.com#about'
+    # job 1. 
+    # Заменить символ “#” на символ “/” в строке 'www.my_site.com#about'
     str_job1 = 'www.my_site.com#about'
     print(str_job1.replace('#', '/'))
 
-    # job 2. Напишите программу, которая добавляет ‘ing’ к словам
+    # job 2. 
+    # Напишите программу, которая добавляет ‘ing’ к словам
     str_job2 = 'test'
     print(str_job2 + 'ing')
 
-    # job 3. В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
+    # job 3. 
+    # В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
     str_job3 = 'Ivanou Ivan'
     print(str_job3[str_job3.find(' ') + 1:len(str_job3)] + ' ' + str_job3[0:str_job3.find(' ')])
 
-    # job 4. Напишите программу которая удаляет пробел в начале, в конце строки
+    # job 4. 
+    # Напишите программу которая удаляет пробел в начале, в конце строки
     str_job4 = '   test   '
     print(str_job4.strip())
 
-    # job 5. Имена собственные всегда начинаются с заглавной буквы, за которой следуют строчные буквы.
-    # Исправьте данное имя собственное так, чтобы оно соответствовало этому утверждению. "pARiS" >> "Paris"
+    # job 5. 
+    # Имена собственные всегда начинаются с заглавной буквы, за которой следуют строчные буквы.
+    # Исправьте данное имя собственное так, чтобы оно соответствовало этому утверждению. 
+    # "pARiS" >> "Paris"
     str_job5 = 'pARiS'
     print(str_job5.title())
 
-    # job 6. Перевести строку в список "Robin Singh" => ["Robin”, “Singh"],
-    # "I love arrays they are my favorite" => ["I", "love", "arrays", "they", "are", "my", "favorite"]
+    # job 6. 
+    # Перевести строку в список "Robin Singh" => ["Robin”, “Singh"],
+    # "I love arrays they are my favorite" => 
+    # ["I", "love", "arrays", "they", "are", "my", "favorite"]
     str_job6_1 = "Robin Singh"
     str_job6_2 = "I love arrays they are my favorite"
     print(str_job6_1.split())
