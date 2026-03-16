90eb0afb20c0deb94603a342cae02a56d79de06b
Andrew <kad.io.work@gmail.com>
2026-03-16 22:42:39 +0300

    add hm7 correct

--- level_up.py
+++ level_up.py
@@ -1,6 +1,12 @@
-def level_up(experience: int, threshold: int, reward: int) -> bool:
+def is_level_up(experience: int, threshold: int, reward: int) -> bool:
+    """
+    :param experience: current XP
+    :param threshold: threshold for level up
+    :param reward: reward for killing monsters
+    :return: will there be level up after killing the monster
+    """
     return experience + reward >= threshold
 
 
-print(level_up(10, 15, 5))
-print(level_up(10, 15, 4))
+print(is_level_up(10, 15, 5))
+print(is_level_up(10, 15, 4))
--- time.py
+++ time.py
@@ -1,4 +1,8 @@
 def time(timer: int) -> int:
+    """
+    :param timer: minutes
+    :return: the sum of the digits shown by the digital timer in the hh:mm format.
+    """
     hours = timer // 60
     minutes = timer % 60
     return hours // 10 + hours % 10 + minutes // 10 + minutes % 10
