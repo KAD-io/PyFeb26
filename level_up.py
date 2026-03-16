8b340203f0eb2e830e3f06f31b48833a123cc563
Andrew <kad.io.work@gmail.com>
2026-03-16 22:50:42 +0300

    add hm7 correct2...maybe

--- level_up.py
+++ level_up.py
@@ -1,10 +1,10 @@
 def is_level_up(experience: int, threshold: int, reward: int) -> bool:
-    """
-    :param experience: current XP
-    :param threshold: threshold for level up
-    :param reward: reward for killing monsters
-    :return: will there be level up after killing the monster
-    """
+
+    # :param experience: current XP
+    # :param threshold: threshold for level up
+    # :param reward: reward for killing monsters
+    # :return: will there be level up after killing the monster
+
     return experience + reward >= threshold
 
 
