3a864f91308e11e00ea40525ff3e4ef0dccc5fee
Andrew <kad.io.work@gmail.com>
2026-03-16 21:35:26 +0300

    add hm7

--- time_converter.py
+++ time_converter.py
@@ -1,8 +1,8 @@
 def time_converter(time24: str) -> str:
-    hours, minutes = time24.split(':')
-    meridiem = 'a.m.' if int(hours) < 12 else 'p.m.'
-    hours = int(hours) % 12 if int(hours) % 12 != 0 else 12
-    return f'{hours}:{minutes} {meridiem}'
+    hours24, minutes = time24.split(':')
+    meridiem = 'a.m.' if int(hours24) < 12 else 'p.m.'
+    hours12 = int(hours24) % 12 if int(hours24) % 12 != 0 else 12
+    return f'{hours12}:{minutes} {meridiem}'
 
 
 print(time_converter('12:30'))
