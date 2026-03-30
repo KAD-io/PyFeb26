059542740d08bba4ead94e1cab5b6cae3d6cd717
Andrew <kad.io.work@gmail.com>
2026-03-30 23:45:30 +0300

    add hm11

--- return_number.py
+++ return_number.py
@@ -5,7 +5,8 @@ def is_number(func):
     def wrapper(*args):
         result = func(*args)
         if not isinstance(result, (int, float)):
-            print(f"Error: The {func.__name__} function returned {type(result).__name__} instead of a number.")
+            print(f"Error: The {func.__name__} function returned {type(result).__name__} "
+                  "instead of a number.")
             return None
         return result
     return wrapper
