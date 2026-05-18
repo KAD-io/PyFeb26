02d5d1ca784bd59dc52b3f1763e28a38d5c5a624
Andrew <kad.io.work@gmail.com>
2026-05-18 23:21:10 +0300

    add hm23

---
+++ test_saucedemo.py
@@ -0,0 +1,71 @@
+"""
+    Login page
+
+        Go to 'https://www.saucedemo.com/'
+
+        Entering username
+        #user-name
+
+        Entering password
+        #password
+
+        Click 'Login'
+        #login-button
+
+    ---------------------------------------------------------------------
+
+    Home page
+
+        Click 'Add to cart': Sauce Labs Backpack
+        #add-to-cart-sauce-labs-backpack
+
+        Click 'Shopping cart'
+        #shopping_cart_container
+
+    ---------------------------------------------------------------------
+
+    Shopping cart
+
+        Click 'Checkout'
+        #checkout
+
+    ---------------------------------------------------------------------
+
+    Checkout: Your Information
+
+        Entering first name
+        #first-name
+
+        Entering last name
+        #last-name
+
+        Entering ZIP/Postal Code
+        #postal-code
+
+        Click 'Continue'
+        #continue
+
+    ---------------------------------------------------------------------
+
+    Checkout: Overview
+
+        Click 'Finish'
+        #finish
+
+    ---------------------------------------------------------------------
+
+    Checkout: Complete!
+
+        Click 'Back Home'
+        #back-to-products
+
+    ---------------------------------------------------------------------
+
+    Home page
+
+        Click 'Burger menu'
+        #react-burger-menu-btn
+
+        Click 'Logout'
+        #logout_sidebar_link
+"""
