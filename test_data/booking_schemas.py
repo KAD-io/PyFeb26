7e5570de262bd6d7f8fbd9843c579f43087a0f8e
Andrew <kad.io.work@gmail.com>
2026-06-15 03:41:23 +0300

    add hm31

---
+++ test_data/booking_schemas.py
@@ -0,0 +1,38 @@
+class Schema:
+    AUTH = {
+        "type": "object",
+        "properties": {
+            "token": {"type": "string"}
+        },
+        "required": ["token"]
+    }
+
+    BOOKING = {
+        "type": "object",
+        "properties": {
+            "firstname": {"type": "string"},
+            "lastname": {"type": "string"},
+            "totalprice": {"type": "integer"},
+            "depositpaid": {"type": "boolean"},
+            "bookingdates": {
+                "type": "object",
+                "properties": {
+                    "checkin": {"type": "string", "format": "date"},
+                    "checkout": {"type": "string", "format": "date"}
+                },
+                "required": ["checkin", "checkout"]
+            },
+            "additionalneeds": {"type": "string"}
+        },
+        "required": ["firstname", "lastname", "totalprice", "depositpaid", "bookingdates"]
+    }
+
+    CREATE_BOOKING_RESPONSE = {
+        "type": "object",
+        "properties": {
+            "bookingid": {"type": "integer"},
+            "booking": BOOKING
+        },
+        "required": ["bookingid", "booking"]
+    }
+    
